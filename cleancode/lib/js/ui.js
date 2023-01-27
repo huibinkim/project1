53d2bb7a87a830ff8c8da9e420af9d2f4caf35ec
[huibinkim] <[abc00127@naver.com]>
2023-01-27 16:57:15 +0900

    추가 수정 js

--- cleancode/lib/js/ui.js
+++ cleancode/lib/js/ui.js
@@ -1,22 +1,44 @@
 $(document).ready(function(){
   // 2depth sub-menu open
-    $('.sub-menu').click(function(){
-    $(this).children('ul').toggle('active');
-  });
-  
-  // gnb 메뉴 아이콘 클릭시, menu active & dim 처리
-  $('.menu-btn').on('click', function(){
-    $('.nav').addClass('show');
-    $('.gnb-overlay').addClass('active');
-  });
-  $('.close-btn').on('click', function(){
-    $('.nav').removeClass('show');
-    $('.gnb-overlay').removeClass('active');
-  })
-  $('.gnb-overlay').on('click', function(){
+    //   $('.sub-menu').click(function(){
+    //   $(this).children('ul').toggle('active');
+    // });
+    
+    // gnb 메뉴 아이콘 클릭시, menu active & dim 처리
+    $('.menu-btn').on('click', function(){
+      $('.nav').addClass('show');
+      $('.gnb-overlay').addClass('active');
+    });
+    $('.close-btn').on('click', function(){
       $('.nav').removeClass('show');
       $('.gnb-overlay').removeClass('active');
     })
+    $('.gnb-overlay').on('click', function(){
+        $('.nav').removeClass('show');
+        $('.gnb-overlay').removeClass('active');
+    })
+
+    // console.log($('.nav ul > li > ul ').length);
+    $('.nav ul > li > a').click(function(){
+      $target = $(this).next('ul');
+      var allPanels = $('.nav ul > li > ul');
+
+      if($(this).siblings('ul').length > 0){
+
+        if( !$target.hasClass('down')) {
+          allPanels.slideUp().removeClass('down')
+          $target.slideDown().addClass('down')
+          $target.parents('li').addClass('minus');
+        } else {
+          $target.parents('li').removeClass('minus');
+          $target.slideUp().removeClass('down')
+          
+        }
+      }
+      
+    });
+
+  
 })
 
 
